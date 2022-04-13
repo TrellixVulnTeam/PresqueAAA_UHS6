9f535fbbc009fe905a409cd1d94cef2718c94351
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-04-13 10:31:22 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.get('/user', function (req, res) {
+    res.status(200).json({ name: 'john' });
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// Test "User"
+request(app)
+    .get('/user')
+    .expect('Content-Type', /json/)
+    .expect('Content-Length', '15')
+    .expect(200)
+    .end(function (err, res) {
+        if (err) throw err;
+    });
+
+// Test "Hello World"
+request(app)
+    .get('/')
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .expect('Content-Length', '12')
+    .expect(200)
+    .end(function (err, res) {
+        if (err) throw err;
+    });
